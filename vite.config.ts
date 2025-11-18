ab17cbefa228822c8c41782a799ae04850415b2b
DarkRiters <bartoszbiel@studenci.collegiumwitelona.pl>
2025-11-18 01:15:47 +0100

    Small changes for vite env port

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 5173,
-    host: true,
-  },
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [vue()],
+    server: {
+      port: Number(env.PORT),
+      host: true,
+    },
+  };
 });
